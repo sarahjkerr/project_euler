7956b92f4a924e000685d9da5e7a0085b86347b4
Sarah K <sarah.j.kerr10@gmail.com>
2019-09-17 10:56:01 -0400

    Update 5_smallest_multiple.py

--- 5_smallest_multiple.py
+++ 5_smallest_multiple.py
@@ -1,10 +1,10 @@
 #Can reduce full list to check, since if a number is divisible by 18, it's also divisible by 6 and 3, and so on and so on
+#Can also start at 2520 and increment up by 20
 
 nums = [11, 13, 14, 16, 17, 18, 19, 20]
-answer_list = []
 
-for x in range(2520, 99999999, 20):
+for x in range(2520, 9999999999, 20):
     if all(x % y == 0 for y in nums):
-        answer_list.append(x)
+        print(x)
         
-min(answer_list)
+#232792560
